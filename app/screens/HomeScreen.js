bf67a5718012e53bc76fb6e0b0ae3780fca5f715
syzhang <sigridzhang0603@163.com>
2017-09-11 17:05:06 +0800

    production list screen

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,21 +1,86 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
+    Container,
+    Header,
+    Title,
+    Content,
+    List,
+    ListItem,
+    Body,
+    Icon,
     Text,
-    View,
-    Button,
-} from 'react-native';
+    Footer,
+    Button
+} from 'native-base';
+import {Image} from 'react-native';
+
+const items = [
+    {
+        "name": "The Shawshank Redemption",
+        "poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1_SY1000_CR0,0,672,1000_AL_.jpg",
+    },
+    {
+        "name": "The Godfather",
+        "poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BZTRmNjQ1ZDYtNDgzMy00OGE0LWE4N2YtNTkzNWQ5ZDhlNGJmL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY268_CR3,0,182,268_AL_.jpg",
+    },
+    {
+        "name": "The Godfather: Part II",
+        "poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BMjZiNzIxNTQtNDc5Zi00YWY1LThkMTctMDgzYjY4YjI1YmQyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY268_CR3,0,182,268_AL_.jpg",
+    },
+    {
+        "name": "The Dark Night",
+        "poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    },
+    {
+        "name": "12 Angry Men",
+        "poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BYzNlNWRmNjgtYWZlOS00MTQ1LTk5MjItODFmNThjNzM1MTM5L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    },
+    {
+        "name": "Schindler's List",
+        "poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BNDE4OTMxMTctNmRhYy00NWE2LTg3YzItYTk3M2UwOTU5Njg4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    },
+    {
+        "name":"Pulp Fiction",
+        "poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTkxMTA5OTAzMl5BMl5BanBnXkFtZTgwNjA5MDc3NjE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    },
+]
+
 
 class HomeScreen extends React.Component {
     static navigationOptions = {
         tabBarLabel: 'Home',
+        tabBarIcon: ({tintColor}) => (
+            <Icon
+                name="home"
+                style={[{fontSize: 25}, {color: tintColor}]}
+            />
+        ),
     };
 
     render() {
         return (
-            <Button
-                onPress={() => this.props.navigation.navigate('OrderList')}
-                title="Go to Orders List"
-            />
+            <Container>
+                <Header>
+                    <Body>
+                    <Title>Items</Title>
+                    </Body>
+                </Header>
+                <Content>
+                    <List>
+                        {items.map((item, i) => (<ListItem key={i}>
+                                    <Image style={{width: 100, height: 120}} source={{uri: `${item.poster}`}}/>
+                                    <Body style={{flex: 1}}>
+                                    <Text>{item.name}</Text>
+                                    <Button iconLeft transparent primary>
+                                        <Icon name='cart'/>
+                                    </Button>
+                                    </Body>
+                                </ListItem>
+                            )
+                        )}
+                    </List>
+                </Content>
+            </Container>
         );
     }
 }
--- app/screens/OrdersListScreen.js
+++ app/screens/OrdersListScreen.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
-import {
-    Text,
-    View,
-    Button,
-} from 'react-native';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Body, Icon, Text } from 'native-base';
 
 class OrdersListScreen extends React.Component {
     static navigationOptions = {
         tabBarLabel: 'Orders',
+        tabBarIcon: ({ tintColor }) => (
+            <Icon
+                theme={{ iconFamily: 'FontAwesome' }}
+                name="list"
+                style={[{fontSize:25}, {color: tintColor}]}
+            />
+        ),
     };
 
     render() {
         return (
-            <Button
-                onPress={() => this.props.navigation.navigate('Home')}
-                title="Go to Home"
-            />
+            <Container>
+                <Header>
+                    <Body>
+                    <Title>Order List</Title>
+                    </Body>
+                </Header>
+                <Content>
+                    <Text>
+                        This is Content Section
+                    </Text>
+                </Content>
+            </Container>
         );
     }
 }
